3b37c3ba5cfa4b0d5249f33001220c9606b5f085
YunusYld <yunusyld7@gmail.com>
2024-05-20 20:23:52 +0300

    Updated Folder Structure

---
+++ Assets/Scripts/Core/Runtime/Shared/SomeNonMonoBehaviourThing.cs
@@ -0,0 +1,18 @@
+public sealed partial class SomeNonMonoBehaviourThing
+{
+    // Initialize
+
+
+    // Update
+
+
+    // Dispose
+}
+
+
+#if UNITY_EDITOR
+
+public sealed partial class SomeNonMonoBehaviourThing
+{ }
+
+#endif
---
+++ Assets/Scripts/Core/Runtime/Singletons/Base/MonoBehaviourSingletonBase.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public abstract partial class MonoBehaviourSingletonBase : MonoBehaviour
+{
+    // Initialize
+
+
+    // Update
+
+
+    // Dispose
+}
+
+
+#if UNITY_EDITOR
+
+public abstract partial class MonoBehaviourSingletonBase
+{ }
+
+#endif
